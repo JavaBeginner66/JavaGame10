d561d006c5efb2170fcc258c057a00f0d5219e44
Jan Andreas <jan-anan@hotmail.com>
2019-03-19 17:02:12 +0100

    GameEngineCallbackGUI constructor, get method

--- src/view/observers/GameEngineCallbackGUI.java
+++ src/view/observers/GameEngineCallbackGUI.java
@@ -1,4 +1,16 @@
 package view.observers;
 
+import view.MainFrame;
+
 public class GameEngineCallbackGUI {
+
+    private MainFrame mainFrame;
+
+    public GameEngineCallbackGUI(MainFrame frame){
+        this.mainFrame = frame;
+    }
+
+    public MainFrame getMainFrame() {
+        return mainFrame;
+    }
 }
