91d2d93395f79ab99a110563c89bac0a257b75cc
Jan Andreas <jan-anan@hotmail.com>
2019-03-19 17:02:31 +0100

    Ressource panel extended

--- src/view/RessourcePanel.java
+++ src/view/RessourcePanel.java
@@ -1,14 +1,29 @@
 package view;
 
 import controller.RessourcePanelController;
-import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import model.interfaces.GameEngine;
 
-public class RessourcePanel extends Pane {
+public class RessourcePanel extends GridPane {
+
+    private int gold;
 
     public RessourcePanel(){
+        Label gold = new Label("Gold: ");
+        gold.setFont(Font.font("Monospace",
+                FontWeight.BOLD, FontPosture.REGULAR, 20));
+        this.setStyle("-fx-background-color: #c4c4c4;");
+        this.setPadding(new Insets(0,5,5,15));
 
+        this.add(gold, 0,0);
     }
+
+
 
 
     public void addListeners(MainFrame frame, GameEngine engine){
