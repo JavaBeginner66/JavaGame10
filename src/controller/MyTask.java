98a72292ce3fc86b4469348b05539c031a96e689
Jan Andreas <jan-anan@hotmail.com>
2019-03-21 16:20:04 +0100

    MyTask adjusted to accept null

--- src/controller/MyTask.java
+++ src/controller/MyTask.java
@@ -17,15 +17,13 @@ public class MyTask extends Task<Void> {
     private long parameter;
     private Button b;
     private MainFrame frame;
-    private GameEngine gameEngine;
 
     private ProgressBar bar;
 
-    public MyTask(long parameter, Button b, MainFrame frame, GameEngine gameEngine){
+    public MyTask(long parameter, Button b, MainFrame frame){
         this.parameter = parameter;
         this.b = b;
         this.frame = frame;
-        this.gameEngine = gameEngine;
 
         bar = frame.getEventPanel().addProgressBar();
         bar.progressProperty().bind(this.progressProperty());
@@ -34,27 +32,30 @@ public class MyTask extends Task<Void> {
 
     }
 
-    public Void call(){
+    public Void call() {
 
         try {
-            b.setDisable(true);
+            if(b != null)
+                b.setDisable(true);
             for (long i = 1; i <= parameter; i++) {
                 updateProgress(i, parameter);
                 Thread.sleep(10);
             }
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            b.setDisable(false);
+        } finally {
+            if(b != null)
+                b.setDisable(false);
             Platform.runLater(() -> {
                 frame.getEventPanel().getBarPane().getChildren().remove(bar);
-                gameEngine.progressBarResult(parameter);
             });
-
-
         }
         return null;
+    }
+
+    public long getParameter() {
+        return parameter;
     }
 }
