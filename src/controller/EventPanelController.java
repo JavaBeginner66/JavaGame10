82b5908aeedb886d4256d97de67028cabd866250
Jan Andreas <jan-anan@hotmail.com>
2019-03-27 22:18:22 +0100

    Fixed duplicate threads bug

--- src/controller/EventPanelController.java
+++ src/controller/EventPanelController.java
@@ -37,6 +37,7 @@ public class EventPanelController implements EventHandler<ActionEvent> {
     public EventPanelController(MainFrame frame, GameEngine engine){
         this.frame = frame;
         this.engine = engine;
+        monitorThread();
     }
 
 
@@ -47,59 +48,8 @@ public class EventPanelController implements EventHandler<ActionEvent> {
         Button b = (Button) event.getSource();
         String s = b.getText();
 
-        /* Monitor Thread */
-        if(tasks.isEmpty()){
-            Runnable monitor = new Runnable() {
 
-                @Override
-                public void run() {
-                    boolean running = true;
-                    while(running) {
-                            List<MyTask> tasksToRun = new ArrayList<>(tasks);
-                            for (MyTask task : tasksToRun) {
-                                Platform.runLater(() -> {
-                                    if(task.getParameter() == BASIC_ATTACK && task.isDone() && autoBasicUnlocked) {
 
-                                        executeAutoAttack(null);
-
-                                        /* Attempt to remove duplicate tasks
-                                        for (int i = 0; i < tasks.size(); i++) {
-                                            if (tasks.get(i).getParameter() == BASIC_ATTACK) {
-                                                System.out.println(tasks.remove(tasks.get(i)));
-                                            }
-                                        }
-                                        */
-                                    }
-
-                                    if(task.getParameter() == AUTO_ATTACK && task.isDone()){
-                                        tasks.remove(task);
-                                        executeAutoAttack(frame.getEventPanel().getAttack());
-                                        autoBasicUnlocked = true;
-                                    }
-                                    if (task.isDone()) {
-
-                                        MyTask temp = null;
-
-                                        if (tasks.remove(task))
-                                            temp = task;
-                                        /* Stops multiple of same tasks from getting through and giving gold for completion*/
-                                        if (temp != null)
-                                            if (temp.equals(task))
-                                                engine.progressBarResult(task.getParameter());
-                                    }
-                                });
-                            }
-                            try {
-                                Thread.sleep(30);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                    }
-                }
-            };
-            Thread m = new Thread(monitor);
-            m.start();
-        }
 
         switch(s){
             case "Attack":
@@ -140,6 +90,50 @@ public class EventPanelController implements EventHandler<ActionEvent> {
         executor.execute(task4);
     }
 
+    private void monitorThread(){
+        Runnable monitor = new Runnable() {
 
+            @Override
+            public void run() {
+                boolean running = true;
+                while(running) {
+                    List<MyTask> tasksToRun = new ArrayList<>(tasks);
+                    for (MyTask task : tasksToRun) {
+
+                        if(task.getParameter() == BASIC_ATTACK && task.isDone() && autoBasicUnlocked)
+                            executeAutoAttack(null);
+
+
+                        if(task.getParameter() == AUTO_ATTACK && task.isDone()){
+                            tasks.remove(task);
+                            executeAutoAttack(frame.getEventPanel().getAttack());
+                            autoBasicUnlocked = true;
+                        }
+                        if (task.isDone()) {
+
+                            MyTask temp = null;
+
+                            if (tasks.remove(task))
+                                temp = task;
+                            /* Stops multiple of same tasks from getting through and giving gold for completion*/
+                            if (temp != null)
+                                if (temp.equals(task)) {
+                                Platform.runLater(() ->{
+                                    engine.progressBarResult(task.getParameter());
+                                }); 
+                            }
+                        }
+                    }
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Thread m = new Thread(monitor);
+        m.start();
+    }
 
 }
