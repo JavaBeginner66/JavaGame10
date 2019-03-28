3eecaf6002cc54f68938100a03b471a984e7c2c9
Jan Andreas <jan-anan@hotmail.com>
2019-03-28 17:56:17 +0100

    Moved code to gameEngineImplement

--- src/controller/EventPanelController.java
+++ src/controller/EventPanelController.java
@@ -21,23 +21,12 @@ import java.util.concurrent.RejectedExecutionException;
 
 public class EventPanelController implements EventHandler<ActionEvent> {
 
-    private ExecutorService executor = Executors.newCachedThreadPool();
-    public static ArrayList<MyTask> tasks = new ArrayList<>();
-
-    private final int BASIC_ATTACK = 100;
-    private final int SECONDS_3 = 300;
-    private final int SECONDS_5 = 500;
-    private final int AUTO_ATTACK = 150;
-
-    public static boolean autoBasicUnlocked = false;
-
     private MainFrame frame;
     private GameEngine engine;
 
     public EventPanelController(MainFrame frame, GameEngine engine){
         this.frame = frame;
         this.engine = engine;
-        monitorThread();
     }
 
 
@@ -48,92 +37,21 @@ public class EventPanelController implements EventHandler<ActionEvent> {
         Button b = (Button) event.getSource();
         String s = b.getText();
 
-
-
-
         switch(s){
             case "Attack":
-                executeAutoAttack(frame.getEventPanel().getAttack());
+                engine.executeAutoAttack(frame.getEventPanel().getAttack());
                 break;
             case "Button2":
-                MyTask task2 = new MyTask(SECONDS_3, b, frame);
-                tasks.add(task2);
-                executor.execute(task2);
+
                 break;
             case "Button3":
-                MyTask task3 = new MyTask(SECONDS_5, b, frame);
-                tasks.add(task3);
-                executor.execute(task3);
+
                 break;
             case "Auto-attack: (10 000)":
-                unlockAutoAttack();
+                engine.unlockAutoAttack();
                 break;
         }
     }
 
-    private void executeAutoAttack(Button b){
-        MyTask task = new MyTask(BASIC_ATTACK, b, frame);
-
-        tasks.add(task);
-        frame.getEventPanel().getAttack().setDisable(true);
-
-        executor.execute(task);
-    }
-
-    private void unlockAutoAttack(){
-        MyTask task4 = new MyTask(AUTO_ATTACK, null, frame);
-
-        tasks.add(task4);
-        frame.getEventPanel().getAttack().setDisable(true);
-        frame.getEventPanel().getAutoAttack().setDisable(true);
-
-        executor.execute(task4);
-    }
-
-    private void monitorThread(){
-        Runnable monitor = new Runnable() {
-
-            @Override
-            public void run() {
-                boolean running = true;
-                while(running) {
-                    List<MyTask> tasksToRun = new ArrayList<>(tasks);
-                    for (MyTask task : tasksToRun) {
-
-                        if(task.getParameter() == BASIC_ATTACK && task.isDone() && autoBasicUnlocked)
-                            executeAutoAttack(null);
-
-
-                        if(task.getParameter() == AUTO_ATTACK && task.isDone()){
-                            tasks.remove(task);
-                            executeAutoAttack(frame.getEventPanel().getAttack());
-                            autoBasicUnlocked = true;
-                        }
-                        if (task.isDone()) {
-
-                            MyTask temp = null;
-
-                            if (tasks.remove(task))
-                                temp = task;
-                            /* Stops multiple of same tasks from getting through and giving gold for completion*/
-                            if (temp != null)
-                                if (temp.equals(task)) {
-                                Platform.runLater(() ->{
-                                    engine.progressBarResult(task.getParameter());
-                                }); 
-                            }
-                        }
-                    }
-                    try {
-                        Thread.sleep(30);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-        Thread m = new Thread(monitor);
-        m.start();
-    }
 
 }
--- src/model/GameEngineImplement.java
+++ src/model/GameEngineImplement.java
@@ -1,15 +1,104 @@
 package model;
 
+import controller.MyTask;
+import javafx.application.Platform;
+import javafx.scene.control.Button;
 import model.interfaces.GameEngine;
 import view.observers.GameEngineCallbackGUI;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class GameEngineImplement implements GameEngine {
 
     private GameEngineCallbackGUI callBackGUI;
 
+    private ExecutorService executor = Executors.newCachedThreadPool();
+    public static ArrayList<MyTask> tasks = new ArrayList<>();
+
+    public static boolean autoBasicUnlocked = false;
+
+    private final int BASIC_ATTACK = 100;
+    private final int SECONDS_3 = 300;
+    private final int SECONDS_5 = 500;
+    private final int AUTO_ATTACK = 150;
+
     public GameEngineImplement(GameEngineCallbackGUI callBackGUI){
         this.callBackGUI = callBackGUI;
+        monitorThread();
     }
+
+    @Override
+    public void monitorThread(){
+        Runnable monitor = new Runnable() {
+
+            @Override
+            public void run() {
+                boolean running = true;
+                while(running) {
+                    List<MyTask> tasksToRun = new ArrayList<>(tasks);
+                    for (MyTask task : tasksToRun) {
+
+                        if(task.getParameter() == BASIC_ATTACK && task.isDone() && autoBasicUnlocked)
+                            executeAutoAttack(null);
+
+
+                        if(task.getParameter() == AUTO_ATTACK && task.isDone()){
+                            tasks.remove(task);
+                            executeAutoAttack(callBackGUI.getMainFrame().getEventPanel().getAttack());
+                            autoBasicUnlocked = true;
+                        }
+                        if (task.isDone()) {
+
+                            MyTask temp = null;
+
+                            if (tasks.remove(task))
+                                temp = task;
+                            /* Stops multiple of same tasks from getting through and giving gold for completion*/
+                            if (temp != null)
+                                if (temp.equals(task)) {
+                                    Platform.runLater(() ->{
+                                        progressBarResult(task.getParameter());
+                                    });
+                                }
+                        }
+                    }
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Thread m = new Thread(monitor);
+        m.start();
+    }
+
+    @Override
+    public void executeAutoAttack(Button b){
+        MyTask task = new MyTask(BASIC_ATTACK, b, callBackGUI.getMainFrame());
+
+        tasks.add(task);
+        // Shorten reference
+        callBackGUI.getMainFrame().getEventPanel().getAttack().setDisable(true);
+
+        executor.execute(task);
+    }
+
+    @Override
+    public void unlockAutoAttack(){
+        MyTask task4 = new MyTask(AUTO_ATTACK, null, callBackGUI.getMainFrame());
+
+        tasks.add(task4);
+        callBackGUI.getMainFrame().getEventPanel().getAttack().setDisable(true);
+        callBackGUI.getMainFrame().getEventPanel().getAutoAttack().setDisable(true);
+
+        executor.execute(task4);
+    }
+
 
     @Override
     public void progressBarResult(long time){
