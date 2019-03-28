ef7419a7f958bc760a9ff7343fa92b9bc50f2e3c
Jan Andreas <jan-anan@hotmail.com>
2019-03-28 16:06:49 +0100

    Menupanel done

--- src/view/MenuPanel.java
+++ src/view/MenuPanel.java
@@ -1,20 +1,49 @@
 package view;
 
 import controller.MenuPanelController;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import model.interfaces.GameEngine;
 
-public class MenuPanel extends VBox {
+public class MenuPanel extends MenuBar {
+
+    private Menu menu, help;
+    private MenuItem exit, save, load, newGame, about, shortCuts, description;
 
     public MenuPanel(){
-        Label l = new Label("Noe");
-        this.getChildren().addAll(l);
+
+        /* Menus */
+        menu = new Menu("File");
+        help = new Menu("Help");
+
+        /* Menu items */
+        exit = new MenuItem("Exit Game");
+        save = new MenuItem("Save Game");
+        load = new MenuItem("Load Game");
+        newGame = new MenuItem("New Game");
+
+        about = new MenuItem("About");
+        shortCuts = new MenuItem("Shortcuts");
+        description = new MenuItem("Description");
+
+        /* Construct menu */
+        menu.getItems().addAll(save, load, newGame, exit);
+        help.getItems().addAll(about, shortCuts, description);
+        this.getMenus().addAll(menu, help);
+
         this.setStyle("-fx-background-color: #c4c4c4;");
     }
 
 
     public void addListeners(MainFrame frame, GameEngine engine){
         MenuPanelController listener = new MenuPanelController(frame, engine);
+        exit.setOnAction(listener);
+        save.setOnAction(listener);
+        load.setOnAction(listener);
+        newGame.setOnAction(listener);
+        about.setOnAction(listener);
+        shortCuts.setOnAction(listener);
+        description.setOnAction(listener);
     }
 }
