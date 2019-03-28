5d33dec2d2b773c12004d737f6138d0ada7e41d7
Jan Andreas <jan-anan@hotmail.com>
2019-03-28 18:08:58 +0100

    GameEngine interface complemented

--- src/model/interfaces/GameEngine.java
+++ src/model/interfaces/GameEngine.java
@@ -1,6 +1,36 @@
 package model.interfaces;
 
+import javafx.scene.control.Button;
+
+/**
+ * GameEngine interface
+ * Implementing classes; GameEngineImplement
+ */
+
 public interface GameEngine {
+
+    /**
+     *  Thread that watches over tasks.
+     *  Primarly used to spot whenever a thread is finished
+     *  to give rewards to player.
+     */
+    void monitorThread();
+
+    /**
+     *
+     * Method executeAttack starts the fastest Task thread.
+     *
+     *  @param b
+     *      b is the button reference sent from the EventHandler.
+     *      Used to disable/enable buttons before/after threads.
+     */
+    void executeAutoAttack(Button b);
+
+    /**
+     * Method unlockAutoAttack creates a Task thread that makes the executeAutoAttack Task
+     * automated on completion.
+     */
+    void unlockAutoAttack();
 
     /**
      *
@@ -8,4 +38,5 @@ public interface GameEngine {
      *          How much time the progressbar spent
      */
     void progressBarResult(long time);
+
 }
