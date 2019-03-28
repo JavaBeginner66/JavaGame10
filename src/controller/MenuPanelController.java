a929acf598f8e3bd625d91a827715ad79870a291
Jan Andreas <jan-anan@hotmail.com>
2019-03-28 17:56:24 +0100

    MenuPanelController setup

--- src/controller/MenuPanelController.java
+++ src/controller/MenuPanelController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.MenuItem;
 import model.interfaces.GameEngine;
 import view.MainFrame;
 
-public class MenuPanelController {
+public class MenuPanelController implements EventHandler<ActionEvent> {
 
     private MainFrame frame;
     private GameEngine engine;
@@ -11,5 +14,56 @@ public class MenuPanelController {
     public MenuPanelController(MainFrame frame, GameEngine engine){
         this.frame = frame;
         this.engine = engine;
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        MenuItem menuItem = (MenuItem) event.getSource();
+        String item = (String) menuItem.getText();
+        switch (item){
+            case "Exit Game":
+                exit();
+                break;
+            case "Save Game":
+                save();
+                break;
+            case "Load Game":
+                load();
+                break;
+            case "New Game":
+                newGame();
+                break;
+            case "Description":
+                description();
+                break;
+            case "About":
+                about();
+                break;
+            case "Shortcuts":
+                shortcuts();
+                break;
+        }
+    }
+
+    private void exit(){
+
+    }
+    private void save(){
+
+    }
+    private void load(){
+
+    }
+    private void newGame(){
+
+    }
+    private void description(){
+
+    }
+    private void about(){
+
+    }
+    private void shortcuts(){
+
     }
 }
