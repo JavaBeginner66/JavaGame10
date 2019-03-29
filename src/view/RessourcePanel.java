6185ed78dd430fe388b35fd777007e8009564e85
Jan Andreas <jan-anan@hotmail.com>
2019-03-29 21:46:45 +0100

    Ressource panel extended

--- src/view/RessourcePanel.java
+++ src/view/RessourcePanel.java
@@ -4,27 +4,35 @@ import controller.RessourcePanelController;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 import model.interfaces.GameEngine;
+import view.observers.GameEngineCallbackGUI;
 
 public class RessourcePanel extends GridPane {
 
     private long gold;
+    private long mana;
+
     private Label goldLabel;
+    private Label manaLabel;
 
     public RessourcePanel(){
-        goldLabel = new Label("Gold: ");
-        goldLabel.setFont(Font.font("Monospace",
-                FontWeight.BOLD, FontPosture.REGULAR, 20));
         this.setStyle("-fx-background-color: #c4c4c4;");
         this.setPadding(new Insets(0,5,5,15));
-
-        this.add(goldLabel, 0,0);
+        addLabels();
     }
 
+    private void addLabels(){
 
+        goldLabel = new Label("Gold: ");
+        goldLabel = GameEngineCallbackGUI.setLabelStyle(goldLabel, 20);
+
+        manaLabel = new Label("Mana: ");
+        manaLabel = GameEngineCallbackGUI.setLabelStyle(manaLabel, 20);
+
+
+        this.add(goldLabel, 0,0);
+        this.add(manaLabel, 0, 1);
+    }
 
 
     public void addListeners(MainFrame frame, GameEngine engine){
@@ -34,6 +42,11 @@ public class RessourcePanel extends GridPane {
     public void setGoldLabel(long gold){
         this.gold += gold;
         this.goldLabel.setText("Gold: " + this.gold);
+    }
+
+    public void setMana(){
+        this.mana += mana;
+        this.manaLabel.setText("Mana: " + this.mana);
     }
 
 
