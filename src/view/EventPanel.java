5d329638876615aaf1ce62cfc43f63f6df403ac3
Jan Andreas <jan-anan@hotmail.com>
2019-03-30 12:41:17 +0100

    ButtonPanel styled

--- src/view/EventPanel.java
+++ src/view/EventPanel.java
@@ -3,9 +3,12 @@ package view;
 import controller.EventPanelController;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import model.interfaces.GameEngine;
 
@@ -45,6 +48,7 @@ public class EventPanel extends BorderPane {
         sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         sp.setPannable(true);
+
     }
 
     private void hashButtons(){
@@ -53,7 +57,7 @@ public class EventPanel extends BorderPane {
         buttons.put("attack", attack = new Button("Attack"));
         buttons.put("b2", b2 = new Button("b2"));
         buttons.put("b3", b3 = new Button("b3"));
-        buttons.put("autoAttack", autoAttack = new Button("Unlock AutoAttack"));
+        buttons.put("autoAttack", autoAttack = new Button("AutoAttack"));
         buttons.put("b4", b4 = new Button("b4"));
         buttons.put("b5", b5 = new Button("b5"));
         buttons.put("b6", b6 = new Button("b6"));
@@ -85,25 +89,68 @@ public class EventPanel extends BorderPane {
 
     private void setButtonStyle(TreeMap buttons){
 
-        Tooltip tp = new Tooltip();
-        tp.setStyle("-fx-font-family: normal bold 2; "
+        String buttonStyle = "-fx-background-color: \n" +
+                "        #090a0c,\n" +
+                "        linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%),\n" +
+                "        linear-gradient(#20262b, #191d22),\n" +
+                "        radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));\n" +
+                "    -fx-background-radius: 5,4,3,5;\n" +
+                "    -fx-background-insets: 0,1,2,0;\n" +
+                "    -fx-text-fill: white;\n" +
+                "    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );\n" +
+                "    -fx-font-family: \"Monospace\";\n" +
+                "    -fx-text-fill: linear-gradient(white, #d0d0d0);\n" +
+                "    -fx-font-size: 14px;\n" +
+                "    -fx-padding: 5 5 5 5;" +
+                "    -fx-hover: dropshadow( one-pass-box , rgba(0,0,0,0.9) , 1, 0.0 , 0 , 1 );";
+
+        String hoverStyle = "-fx-background-color: \n" +
+                "        #b2b8c4,\n" +
+                "        linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%),\n" +
+                "        linear-gradient(#20262b, #191d22),\n" +
+                "        radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));\n" +
+                "    -fx-background-radius: 5,4,3,5;\n" +
+                "    -fx-background-insets: 0,1,2,0;\n" +
+                "    -fx-text-fill: white;\n" +
+                "    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );\n" +
+                "    -fx-font-family: \"Monospace\";\n" +
+                "    -fx-text-fill: linear-gradient(green, #d0d0d0);\n" +
+                "    -fx-font-size: 14px;\n" +
+                "    -fx-padding: 5 5 5 5;" +
+                "    -fx-hover: dropshadow( one-pass-box , rgba(0,0,0,0.9) , 1, 0.0 , 0 , 1 );";
+
+        String tooltipStyle = "-fx-font-family: normal bold 2; "
                 + "-fx-base: #AE3522; "
-                + "-fx-text-fill: orange;");
+                + "-fx-text-fill: orange;";
+
+        Tooltip tp = new Tooltip();
+        tp.setStyle(tooltipStyle);
 
         Set keys = buttons.keySet();
         for (Iterator i = keys.iterator(); i.hasNext();) {
             String s = (String) i.next();
             Button b = (Button) buttons.get(s);
-            b.setStyle("-fx-font: 16 arial; -fx-base: #2c64e8; -fx-font-size: 14px;" +
-                    "-fx-text-fill:white; -fx-font-family: \"Arial Narrow\";" +
-                    "    -fx-font-weight: bold;");
+            b.setStyle(buttonStyle);
+
+
             tp.setText("Get correct info");
 
             b.setTooltip(tp);
 
             b.setMinHeight(35);
-            b.setMinWidth(150);
+            b.setMinWidth(100);
+
+            b.setOnMouseEntered(e->{
+                b.setStyle(hoverStyle);
+            });
+
+            b.setOnMouseExited(e->{
+                b.setStyle(buttonStyle);
+            });
+
         }
+
+
 
 
 
@@ -115,8 +162,8 @@ public class EventPanel extends BorderPane {
         //grid.setStyle("-fx-background-color: #c2b1c1;");
 
         grid.setPadding(new Insets(20,20,20,20));
-        grid.setHgap(8);
-        grid.setVgap(8);
+        grid.setHgap(30);
+        grid.setVgap(15);
 
         grid.add(attack, 0,0);
         grid.add(b2, 0,1);
@@ -131,7 +178,15 @@ public class EventPanel extends BorderPane {
         grid.add(b10, 2,5);
         grid.add(b11, 3,5);
 
+        b6.setVisible(false);
 
+
+        BackgroundImage img = new BackgroundImage(new Image("background.jpg", grid.getWidth(), grid.getHeight(),true,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+
+
+        grid.setBackground(new Background(img));
 
         return grid;
     }
