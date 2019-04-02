67c324bd2a716bf9977656ed9053c5cccfa2c131
Jan Andreas <jan-anan@hotmail.com>
2019-04-02 05:37:15 +0200

    Buffpanel extended

--- src/view/BuffPanel.java
+++ src/view/BuffPanel.java
@@ -1,5 +1,6 @@
 package view;
 
+import controller.BuffPanelController;
 import controller.SidePanelController;
 import javafx.geometry.Insets;
 import javafx.scene.control.CheckBox;
@@ -7,7 +8,17 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import model.interfaces.GameEngine;
 
+import java.util.TreeMap;
+
 public class BuffPanel extends GridPane {
+
+    private CheckBox buff1;
+    private CheckBox buff2;
+    private CheckBox buff3;
+
+    private TreeMap<String, CheckBox> buffs;
+
+    private int goldMultiplier = 10;
 
     public BuffPanel(){
 
@@ -15,18 +26,43 @@ public class BuffPanel extends GridPane {
         this.setPadding(new Insets(10,15,5,5));
         this.setHgap(8);
         this.setVgap(8);
+        addBoxes();
 
+    }
+
+    private void addBoxes(){
         Label l = new Label("Buffs");
-        CheckBox cb1 = new CheckBox("First");
-        CheckBox cb2 = new CheckBox("Second");
+
+        buff1 = new CheckBox("1");
+        buff2 = new CheckBox("2");
+        buff3 = new CheckBox("3");
 
         this.add(l, 0 , 0);
-        this.add(cb1, 0,1);
-        this.add(cb2, 0,2);
+        this.add(buff1, 0,1);
+        this.add(buff2, 0,2);
+        this.add(buff3, 0, 3);
+    }
+
+    private void mapBoxes(){
+        buffs = new TreeMap<>();
+        buffs.put("Buff1", buff1);
+        buffs.put("Buff2", buff2);
+        buffs.put("Buff3", buff3);
     }
 
     public void addListeners(MainFrame frame, GameEngine engine){
-        SidePanelController listener = new SidePanelController(frame, engine);
+        BuffPanelController listener = new BuffPanelController(frame, engine);
+        buff1.setOnAction(listener);
+        buff2.setOnAction(listener);
+        buff3.setOnAction(listener);
     }
 
+    public CheckBox getBuffs(String key){
+        return buffs.get(key);
+    }
+
+    public TreeMap<String, CheckBox> getBuffs() {
+        return buffs;
+    }
+    
 }
