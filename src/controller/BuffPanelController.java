ec411e4134cd9b8bb45cdb90bef39b03b4d801eb
Jan Andreas <jan-anan@hotmail.com>
2019-04-10 13:06:25 +0200

    EnergyDrain thread

--- src/controller/BuffPanelController.java
+++ src/controller/BuffPanelController.java
@@ -1,7 +1,10 @@
 package controller;
 
+import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import model.ValueContainer;
 import model.interfaces.GameEngine;
@@ -31,19 +34,58 @@ public class BuffPanelController implements EventHandler {
                 goldIncrease(state);
                 break;
             case "2":
-                System.out.println("Box 2" + " " + state);
+                energyIncrease(state);
                 break;
             case "3":
                 System.out.println("Box 3" + " " + state);
                 break;
+            case "4":
+                System.out.println("Box 4" + " " + state);
+                break;
+            case "5":
+                System.out.println("Box 5" + " " + state);
+                break;
+
         }
     }
 
     private void goldIncrease(boolean state){
-        if(state)
-            valueContainer.setValue("goldMultiplier", 30);
-        else
-            valueContainer.setValue("goldMultiplier", 10);
+        if(state) {
+            valueContainer.setValue("goldMultiplier", 5);
 
+            Thread energyDrain = new Thread() {
+                public void run() {
+                    while (valueContainer.getValue("goldMultiplier") != 1) {
+                        try {
+                            Platform.runLater(()->{
+                                try {
+                                    if(frame.getRessourcePanel().getEnergy() <= 0){
+                                        throw new GameControllerException("Not enough energy");
+                                    }else{
+                                        frame.getRessourcePanel().setEnergyLabel(-1);
+                                    }
+                                }catch(GameControllerException e) {
+                                    System.out.println(e.getMessage());
+                                    frame.getBuffPanel().getBuffs("buff1").setSelected(false);
+                                    valueContainer.setValue("goldMultiplier", 1);
+                                }
+                            });
+                            sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            };
+            energyDrain.start();
+        }
+        else {
+            valueContainer.setValue("goldMultiplier", 1);
+        }
+
+    }
+
+    private void energyIncrease(boolean state){
+        // Bruke 1 thread for flere metoder?
     }
 }
